c55041eab8be993de05fe4a3ef6fcbdebe51158c
ia rigby <marvinzem@gmail.com>
2024-09-16 18:33:52 +0400

    implement refreshing articles

--- app/home.tsx
+++ app/home.tsx
@@ -1,16 +1,20 @@
-import {StyleSheet, ScrollView, Dimensions, Button} from 'react-native';
+import {StyleSheet, ScrollView, Dimensions} from 'react-native';
 
 import {ThemedText} from '@/components/ThemedText';
 import {ThemedView} from '@/components/ThemedView';
 import {useResource} from "@/hooks/useResource";
 import {Article} from "@/resources/article";
-import React from "react";
+import React, {useState} from "react";
 import {ArticleListItem} from "@/components/ArticleListItem";
+import {FetchArticlesView} from "@/components/FetchArticlesView";
 
 
 // noinspection JSUnusedGlobalSymbols
 export default function HomeScreen() {
-    const [articles, error, isLoading] = useResource<Article[]>('/articles', [])
+    const [loadTime, setLoadTime] = useState<number>(new Date().getTime())
+    const [articles, error, isLoading] = useResource<Article[]>('/articles', [], {loadTime})
+
+    const updateArticles = () => setLoadTime(new Date().getTime())
 
     if (isLoading) {
         return <ThemedView><ThemedText>Loading</ThemedText></ThemedView>
@@ -19,18 +23,13 @@ export default function HomeScreen() {
         return <ThemedView><ThemedText>error: {error}</ThemedText></ThemedView>
     }
 
-    function fetchNewArticles() {
-
-    }
-
     return (<ScrollView style={styles.articlesContainer}>
         <ThemedView style={{height: 40}}></ThemedView>
-        <Button title='fetch new articles' onPress={fetchNewArticles} ></Button>
+        <FetchArticlesView updateArticles={updateArticles}/>
         {articles.map(a => <ArticleListItem key={a.id} article={a}/>)}
     </ScrollView>)
 
 }
-
 
 const styles = StyleSheet.create({
     articlesContainer: {
