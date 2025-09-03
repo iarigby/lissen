2cc44543a60fa1d9db50c84f52a0169588082472
ia mgvdliashvili <ia@ias-MacBook-Air.local>
2025-09-03 14:26:33 +0400

    move to new mac

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,4 +1,4 @@
-import { Text, type TextProps, StyleSheet } from 'react-native';
+import {Text, type TextProps, StyleSheet, TextInput, TextInputProps} from 'react-native';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
 
@@ -30,6 +30,39 @@ export function ThemedText({
       ]}
       {...rest}
     />
+  );
+}
+
+
+export type ThemedTextInputProps = TextInputProps & {
+  lightColor?: string;
+  darkColor?: string;
+  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+};
+
+
+export function ThemedTextInput({
+                             style,
+                             lightColor,
+                             darkColor,
+                             type = 'default',
+                             ...rest
+                           }: ThemedTextInputProps) {
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+
+  return (
+      <TextInput
+          style={[
+            { color },
+            type === 'default' ? styles.default : undefined,
+            type === 'title' ? styles.title : undefined,
+            type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
+            type === 'subtitle' ? styles.subtitle : undefined,
+            type === 'link' ? styles.link : undefined,
+            style,
+          ]}
+          {...rest}
+      />
   );
 }
 
